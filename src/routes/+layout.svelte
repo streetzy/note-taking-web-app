5a52656e9771ab5587e1e0d585c14183b754ff6e
streetzy <57232857+streetzy@users.noreply.github.com>
2024-12-01 20:59:05 +0100

    landing-page and base

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+    import './styles.scss';
+
+    let loggedIn = $state(false);
+    let {children} = $props();
+</script>
+
+<nav class="top-nav">
+    <div class="page-name">
+        <h1>inscribe</h1>
+    </div>
+    <div class="user-nav-menu">
+        <h2>forum</h2>
+        {#if loggedIn}
+        <h2>notebooks</h2>
+        {/if}
+        <div class="user-nav-buttons">
+            {#if !loggedIn}
+            <Icon icon="carbon:user-avatar-filled" width="48" height="48"/>
+            {:else}
+            <Icon icon="carbon:gears" width="48" height="48"/>
+            <Icon icon="solar:exit-linear" width="48" height="48"/>
+            {/if}
+        </div>
+        
+    </div>
+    
+</nav>
+
+<style lang="scss">
+    .page-name {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-left: 2rem;
+    }
+
+    .page-name h1 {
+        font-weight: 400;
+        font-size: 40px;
+        margin: 0;
+    }
+
+    .user-nav-menu {
+        padding-right: 2rem;
+        gap: 7.5rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .user-nav-menu h2 {
+        margin: 0;
+        font-weight: 400;
+        font-size: 32px;
+    }
+
+    .user-nav-buttons {
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+        line-height: 1;
+    }
+
+    .top-nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 100dvw;
+        height: 10dvh;
+    }
+
+</style>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+</script>
+
+
+<div class="content-container">
+    <div class="content">
+        <div class="content-text">
+            <h3>Create notes, graphs, equations without the need of studying how to.</h3>
+            <p>Tired of feeling lost in a complex note-taking environment?</p>
+            <p>Want to efficiently write organized notes?</p>
+            <p>inscribe offers you the solution to your problems.</p>
+        </div>
+        
+        <div class="content-icons">
+            <Icon icon="carbon:logo-discord" width="64" height="64"/>
+            <Icon icon="carbon:logo-github" width="64" height="64"/>
+            <Icon icon="carbon:logo-mastodon" width="64" height="64"/>
+        </div>
+    </div>
+    <footer>
+        <p class="contact">Contact me at adamsindler@gmail.com</p>
+        <p class="copyright">© 2024 Adam Šindler</p>
+    </footer>
+</div>
+
+<style lang="scss">
+
+    p, h3 {
+        margin: 0;
+        font-family: "Exo", system-ui, -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+    sans-serif;
+        font-weight: 200;
+    }
+
+    .content-container {
+        display: grid;
+        grid-template-rows: auto 64px;
+        height: 90dvh;
+    }
+
+    .content-container > * { 
+        width: 100%;
+        height: 100%;
+    }
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 4rem;
+    }
+
+    .content-text > p { 
+        font-size: 28px;
+    }
+
+    .content-text > h3 {
+        font-size: 36px;
+    }
+    
+    .content-text {
+        gap: 4rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        width: 40%;
+    }
+
+    .content-icons {
+        display: flex;
+        flex-direction: row;
+        gap: 5rem;
+    }
+
+    footer {
+        display: flex;
+        justify-content: space-between;
+        align-items:end;
+    }
+
+    footer > p {
+        padding-bottom: 10px;
+    }
+    .contact {
+        padding-left: 50px;
+    }
+    
+    .copyright {
+        padding-right: 50px;
+    }
+</style>
